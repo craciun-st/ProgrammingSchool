82835bb8ba39da67c6a3df0d66c21d9f81c6ca98
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-03-24 19:58:43 +0200

    Add Module enum
    The abstract method nextModule is implemented for each enum type
    (starting backwards from last module!) since we need to declare
    constant types before returning them.

---
+++ src/main/java/com/codecool/programmingschool/Module.java
@@ -0,0 +1,47 @@
+package com.codecool.programmingschool;
+
+public enum Module {
+    ADVANCED (ProgrammingLanguage.JAVA_EE) {
+
+        @Override
+        public Module nextModule() {
+            return null;
+        }
+    },
+
+
+    OOP (ProgrammingLanguage.JAVA_SE) {
+
+        @Override
+        public Module nextModule() {
+            return ADVANCED;
+        }
+    },
+
+
+    WEB (ProgrammingLanguage.WEB) {
+
+        @Override
+        public Module nextModule() {
+            return OOP;
+        }
+    },
+
+
+    PB (ProgrammingLanguage.PYTHON) {
+        @Override
+        public Module nextModule() {
+            return Module.WEB;
+        }
+    };
+
+
+    public ProgrammingLanguage language;
+
+    Module(ProgrammingLanguage language) {
+        this.language = language;
+    }
+
+    public abstract Module nextModule();
+
+}
