de724d434c3c2bc5c90ca6c02299cc57f10d056b
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-03-24 21:13:53 +0200

    Made salary field for Staff nullable and explained constructors

--- src/main/java/com/codecool/programmingschool/Staff.java
+++ src/main/java/com/codecool/programmingschool/Staff.java
@@ -3,18 +3,20 @@ package com.codecool.programmingschool;
 import java.util.Objects;
 
 public abstract class Staff extends Person {
-    private double salary;
+    private Double salary;
 
+    // Staff that has not yet agreed upon a salary (will need to have the Double set)
     public Staff(String name, String phoneNumber, String birthDateAsString) {
         super(name, phoneNumber, birthDateAsString);
     }
 
+    // Staff that has agreed upon a salary
     public Staff(String name, String phoneNumber, String birthDateAsString, double salary) {
         super(name, phoneNumber, birthDateAsString);
         this.salary = salary;
     }
 
-    public double getSalary() {
+    public Double getSalary() {
         return salary;
     }
 
