0b8084ee3ef26ec50d07e67295495cbae9e8138b
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-03-25 12:55:51 +0200

    Add Constants class and new RandomGen methods
    These will be needed to construct new Person objects

---
+++ src/main/java/com/codecool/programmingschool/util/Constants.java
@@ -0,0 +1,71 @@
+package com.codecool.programmingschool.util;
+
+public class Constants {
+    public static final String[] FEMALE_FIRST_NAMES = new String[] {
+            "Alexandra", "Ana", "Ana-Maria", "Aurelia", "Andreea",
+            "Bianca", "Brandusa",
+            "Camelia", "Catalina", "Claudia", "Constanta", "Cristina",
+            "Dana", "Daniela", "Diana", "Domnica", "Dorina", "Dumitra",
+            "Ecaterina", "Elena", "Elisabeta", "Emanuela", "Emilia",
+            "Felicia", "Filofteia", "Floarea", "Florina", "Francesca",
+            "Gabriela", "Georgiana", "Gianina", "Gloria",
+            "Hortensia",
+            "Iasmina", "Ioana", "Ionela", "Irina", "Iulia", "Ivona",
+            "Jeana", "Julieta",
+            "Laura", "Lenuta", "Letitia", "Ligia", "Lorena", "Lucia", "Luiza",
+            "Madalina", "Magdalena", "Margareta", "Maria", "Mihaela", "Monica",
+            "Nadia", "Natalia", "Nicoleta", "Nora",
+            "Oana", "Olga", "Olivia", "Otilia",
+            "Paraschiva", "Patricia", "Paula", "Petruta",
+            "Raluca", "Rebeca", "Rodica", "Roxana", "Ruxandra",
+            "Sabina", "Sandra", "Sanziana", "Silvia", "Sorina", "Stefania", "Suzana",
+            "Tamara", "Teodora", "Tereza", "Tudorita",
+            "Valentina", "Vasilica", "Victoria", "Violeta", "Viviana",
+            "Zamfira", "Zaraza", "Zoe"
+    };
+
+    public static final String[] MALE_FIRST_NAMES = new String[] {
+            "Adam", "Alexandru", "Alin", "Andrei", "Anghel", "Antonie", "Aurelian",
+            "Barbu", "Banel", "Bogdan", "Bucur",
+            "Camil", "Catalin", "Ciprian", "Constantin", "Corneliu", "Costel", "Cristian",
+            "Dan", "Decebal", "Dinu", "Dorin", "Dragos", "Dumitru",
+            "Eduard", "Emanuel", "Emil", "Eremia", "Eugen",
+            "Fabian", "Felix", "Filip", "Florian",
+            "Gabriel", "George", "Gheorghita", "Gicu", "Grigore",
+            "Horatiu", "Horia",
+            "Iacob", "Ieremia", "Ilie", "Ioan", "Ionut", "Iorgu", "Iosif", "Iulian", "Ivan",
+            "Jean",
+            "Laurentiu", "Lazar", "Liviu", "Luca", "Lucian",
+            "Manole", "Marian", "Marius", "Mihai", "Mircea", "Mitica", "Moise",
+            "Nae", "Nelu", "Nichifor", "Nicolae", "Nicusor",
+            "Octav", "Olimpiu", "Ovidiu",
+            "Pamfil", "Paul", "Pavel", "Petru", "Pompiliu",
+            "Radu", "Rares", "Razvan", "Remus", "Robert", "Romeo", "Romulus",
+            "Sandu", "Sebastian", "Serban", "Silviu", "Simion", "Sorin", "Stefan", "Stelian",
+            "Teodor", "Tiberiu", "Titus", "Traian", "Tudor",
+            "Valentin", "Valeriu", "Vasile", "Victor", "Vlad", "Voicu",
+            "Zaharia", "Zamfir"
+    };
+
+    public static final String[] LAST_NAMES = new String[] {
+            "Andrei", "Anghel", "Avram",
+            "Badea", "Balan", "Barbu", "Bogdan", "Bucur",
+            "Ciobanu", "Cojocaru", "Coman", "Constantin", "Constantinescu", "Costea", "Craciun", "Crisan", "Cristea",
+            "David", "Diaconu", "Dinu", "Dobre", "Dragan", "Dragomir", "Dumitrescu", "Dumitru",
+            "Ene", "Enache",
+            "Filip", "Florea",
+            "Georgescu", "Gheorghe", "Grigore",
+            "Iacob", "Iancu", "Ilie", "Ion", "Ionescu", "Ionita", "Iordache", "Ivan",
+            "Kovacs",
+            "Lazar", "Luca", "Lungu", "Lupu",
+            "Manea", "Marcu", "Marin", "Marinescu", "Matei", "Mihai", "Mocanu", "Moldovan", "Munteanu", "Muresan",
+            "Nagy", "Neagu", "Nicolae", "Nistor", "Nita",
+            "Olteanu", "Oprea",
+            "Pavel", "Petre", "Petrescu", "Pop", "Popa", "Popescu", "Preda",
+            "Radu", "Radulescu", "Roman", "Rosu", "Rotaru", "Rus", "Rusu",
+            "Sandu", "Serban", "Simion", "Stan", "Stanciu", "Stefan", "Stoian", "Stoica", "Suciu", "Szabo",
+            "Tanase", "Toma", "Tudor",
+            "Ungureanu",
+            "Vasile", "Vlad", "Voicu"
+    };
+}
--- src/main/java/com/codecool/programmingschool/util/RandomGen.java
+++ src/main/java/com/codecool/programmingschool/util/RandomGen.java
@@ -1,5 +1,6 @@
 package com.codecool.programmingschool.util;
 
+import java.time.LocalDate;
 import java.util.EnumSet;
 import java.util.Random;
 
@@ -26,5 +27,64 @@ public class RandomGen {
 
         // otherwise, we get here when we have a set of size 0
         return null;
+    }
+
+    public static int nextInt(Integer bound) {
+        if (bound != null) {
+            return new Random().nextInt(bound);
+        } else {
+            return new Random().nextInt();
+        }
+    }
+
+    public static String getRandomFemaleFullName() {
+        Random randomizer = new Random();
+        int sizeOfFirstNames = Constants.FEMALE_FIRST_NAMES.length;
+        int sizeOfLastNames = Constants.LAST_NAMES.length;
+        String firstName = Constants.FEMALE_FIRST_NAMES[randomizer.nextInt(sizeOfFirstNames)];
+        String lastName = Constants.LAST_NAMES[randomizer.nextInt(sizeOfLastNames)];
+        return firstName + " " + lastName;
+    }
+
+    public static String getRandomMaleFullName() {
+        Random randomizer = new Random();
+        int sizeOfFirstNames = Constants.MALE_FIRST_NAMES.length;
+        int sizeOfLastNames = Constants.LAST_NAMES.length;
+        String firstName = Constants.MALE_FIRST_NAMES[randomizer.nextInt(sizeOfFirstNames)];
+        String lastName = Constants.LAST_NAMES[randomizer.nextInt(sizeOfLastNames)];
+        return firstName + " " + lastName;
+    }
+
+    public static String getRandomBirthDateStringForAge(int age) {
+        int currentYear = LocalDate.now().getYear();
+        int birthYear = currentYear - age;
+        int birthMonth = RandomGen.nextInt(12) + 1;
+        int birthDay = RandomGen.getRandomIntWithMeanAndStdDev(15, 7);
+
+        return String.format("%d-%d-%d",birthYear, birthMonth, birthDay);
+    }
+
+    public static String getRandomPhoneNumberString() {
+        boolean isMobilePhoneNumber = (RandomGen.nextInt(100) < 50);
+        String prefix;
+        String rest;
+        if (isMobilePhoneNumber) {
+            prefix = String.format("%04d",RandomGen.nextInt(500)+500);
+            rest = String.format(
+                    "%03d %03d",
+                    RandomGen.nextInt(1000),
+                    RandomGen.nextInt(1000)
+            );
+        }
+        else {
+            prefix = "(021)";
+            rest = String.format(
+                    "%03d %02d %02d",
+                    RandomGen.nextInt(1000),
+                    RandomGen.nextInt(100),
+                    RandomGen.nextInt(100)
+            );
+        }
+        return String.format("%s %s", prefix, rest);
     }
 }
